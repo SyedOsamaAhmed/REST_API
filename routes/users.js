7e60d759ab35b80748a27267db07a304f9b8583d
Osama <batch17.026@gmail.com>
2022-06-01 13:16:33 +0500

    deletion of user frrom rest api

--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,34 @@
 import express from "express";
 
+import { v4 as uuidv4 } from "uuid";
+
 const router = express.Router();
 
-const users = [
-  {
-    Firstname: "Osama",
-    Lastname: "Ahmed",
-    age: 23,
-  },
-  {
-    Firstname: "Fahim",
-    Lastname: "Ahmed",
-    age: 27,
-  },
-];
+let users = [];
 
 router.get("/", (req, resp) => {
   resp.send(users);
 });
 
+router.post("/", (req, resp) => {
+  const user = req.body;
+  const userwithId = {
+    ...user,
+    id: uuidv4(),
+  };
+  users.push(userwithId);
+  resp.send(users);
+});
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  const userFound = users.find((user) => user.id === id);
+  res.send(userFound);
+});
+
+router.delete("/:id", (req, res) => {
+  const { id } = req.params;
+  users = users.filter((user) => user.id !== id);
+  res.send(`user with id:${id} is successfully deleted!`);
+});
 export default router;
